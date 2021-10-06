17ff13303d30e7158b8ef02d83365dc9c017d3c6
barisayhan01 <barisayhan01@gmail.com>
2021-10-06 13:39:19 +0300

    Add project files.

---
+++ TrackerUI/TournamentViewerForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace TrackerUI
+{
+	partial class TournamentViewerForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.headerLabel = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// headerLabel
+			// 
+			this.headerLabel.AutoSize = true;
+			this.headerLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+			this.headerLabel.Location = new System.Drawing.Point(38, 35);
+			this.headerLabel.Name = "headerLabel";
+			this.headerLabel.Size = new System.Drawing.Size(165, 38);
+			this.headerLabel.TabIndex = 0;
+			this.headerLabel.Text = "Tournament";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+			this.label1.Location = new System.Drawing.Point(234, 35);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(165, 38);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Tournament";
+			// 
+			// TournamentViewerForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(828, 488);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.headerLabel);
+			this.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.Name = "TournamentViewerForm";
+			this.Text = "Tournament Viewer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label headerLabel;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
